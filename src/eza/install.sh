4f0f4259040f69fa8f191975c16ac58a673f4eef
Rafael Augusto de Oliveira <olirafa@proton.me>
2025-05-14 21:59:58 +0100

    feat: add eza install

---
+++ src/eza/install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+
+echo "Starting to config Eza install..."
+
+mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | tee /etc/apt/sources.list.d/gierens.list
+chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+
+echo "Updating APT packages..."
+apt update
+
+echo "Installing Eza..."
+apt install -y eza
+
+echo "Finished installing Eza!" 
