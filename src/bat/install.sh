3b3513e1d732c14f2f1c1def2dea4706e1a678c8
Rafael Augusto de Oliveira <olirafa@proton.me>
2025-05-14 22:31:54 +0100

    fix(bat): fix alias not working for non-root user

--- src/bat/install.sh
+++ src/bat/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-mkdir -p ~/.local/bin && ln -s /usr/bin/batcat ~/.local/bin/bat
+mkdir -p $_REMOTE_USER_HOME/.local/bin && ln -s /usr/bin/batcat $_REMOTE_USER_HOME/.local/bin/bat
 
 echo "Finished installing Bat!" 
