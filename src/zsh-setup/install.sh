e7f74a06933c330d5a4e1362313eb05d451c4fa6
Rafael Augusto de Oliveira <olirafa@proton.me>
2025-05-15 00:37:45 +0100

    fix(zsh-setup): fix chown command

--- src/zsh-setup/install.sh
+++ src/zsh-setup/install.sh
@@ -14,8 +14,8 @@ wget https://gist.githubusercontent.com/OliRafa/f39d29a34f38edf4a9c9a2f6dd092260
 
 wget https://gist.githubusercontent.com/OliRafa/f39d29a34f38edf4a9c9a2f6dd092260/raw/.zshrc -O $_REMOTE_USER_HOME/.zshrc
 
-chwon $_REMOTE_USER $_REMOTE_USER_HOME/.p10k.zsh
-chwon $_REMOTE_USER $_REMOTE_USER_HOME/.zshenv
-chwon $_REMOTE_USER $_REMOTE_USER_HOME/.zshrc
+chown $_REMOTE_USER $_REMOTE_USER_HOME/.p10k.zsh
+chown $_REMOTE_USER $_REMOTE_USER_HOME/.zshenv
+chown $_REMOTE_USER $_REMOTE_USER_HOME/.zshrc
 
 echo "Finished setting up Zsh!"
