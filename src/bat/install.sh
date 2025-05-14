e37a8491d5f1791e6b06c5741a1bf6ecef298c87
Rafael Augusto de Oliveira <olirafa@proton.me>
2025-05-15 00:50:20 +0100

    fix(bat): fix alias file not being owned by root

--- src/bat/install.sh
+++ src/bat/install.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-mkdir -p $_REMOTE_USER_HOME/.local/bin && ln -s /usr/bin/batcat $_REMOTE_USER_HOME/.local/bin/bat
+mkdir -p $_REMOTE_USER_HOME/.local/bin
+
+ln -s /usr/bin/batcat $_REMOTE_USER_HOME/.local/bin/bat
+
+chown $_REMOTE_USER $_REMOTE_USER_HOME/.local
+chown $_REMOTE_USER $_REMOTE_USER_HOME/.local/**/*
 
 echo "Finished installing Bat!" 
