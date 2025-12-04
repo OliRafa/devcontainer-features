e2fe6d9333b2bf726711a8ae4a60489e19c37627
Rafael Augusto de Oliveira <olirafa@proton.me>
2025-12-04 22:28:12 +0000

    refac: change base to dotfiles with stow

--- src/neovim-setup/install.sh
+++ src/neovim-setup/install.sh
@@ -4,35 +4,13 @@ set -e
 
 echo "Starting to setup Neovim..."
 
-USERNAME="${USERNAME:-"${_REMOTE_USER:-"dev"}"}"
-HOME_DIR=$( getent passwd "$USERNAME" | cut -d: -f6 )
-
-if [ ! -d "$HOME_DIR/.config" ]
-then
-    echo "Creating $HOME_DIR/.config dir..."
-    mkdir -p $HOME_DIR/.config
-else
-    if [ -n "$(ls -A "$HOME_DIR/.config" 2>/dev/null)" ]
-    then
-        echo "Backing up $HOME_DIR/.config dir..."
-        mkdir $HOME_DIR/temp
-        mv $HOME_DIR/.config/* $HOME_DIR/temp/
-    fi
-fi
-
-echo "Downloading DotFiles..."
-git clone https://github.com/OliRafa/neovim-configs.git $HOME_DIR/.config
-
-if [ -d "$HOME_DIR/temp" ]
-then
-    echo "Cleaning up $HOME_DIR/.config dir backup..."
-    mv $HOME_DIR/temp/* $HOME_DIR/.config/
-    rm -r $HOME_DIR/temp
-fi
+echo "Downloading Dotfiles..."
+git clone https://github.com/OliRafa/dotfiles.git 
+cd dotfiles
+stow .
 
 echo "Setting up LazyVim and Mason..."
 nvim --headless "+Lazy! sync" +qa
 
-chown $USERNAME $HOME_DIR/.config/**/*
 
 echo "Finished setting up Neovim!"
