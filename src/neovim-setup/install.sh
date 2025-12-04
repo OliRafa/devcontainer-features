fab4d33ab05311f16cbcb36acc295404dd5cd333
Rafael Augusto de Oliveira <olirafa@proton.me>
2025-12-04 22:55:08 +0000

    fix: not being able to stow

--- src/neovim-setup/install.sh
+++ src/neovim-setup/install.sh
@@ -4,13 +4,16 @@ set -e
 
 echo "Starting to setup Neovim..."
 
+USERNAME="${USERNAME:-"${_REMOTE_USER:-"dev"}"}"
+HOME_DIR=$( getent passwd "$USERNAME" | cut -d: -f6 )
+
 echo "Downloading Dotfiles..."
+cd $HOME_DIR
 git clone https://github.com/OliRafa/dotfiles.git 
 cd dotfiles
 stow .
 
 echo "Setting up LazyVim and Mason..."
 nvim --headless "+Lazy! sync" +qa
-
 
 echo "Finished setting up Neovim!"
