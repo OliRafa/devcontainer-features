7619ecb09dda0e0c9c9672c0a60b92a957ba4bbe
Rafael Augusto de Oliveira <olirafa@proton.me>
2025-12-04 23:17:38 +0000

    fix(neovim-setup): run stow as non-root user

--- src/neovim-setup/install.sh
+++ src/neovim-setup/install.sh
@@ -8,14 +8,16 @@ USERNAME="${USERNAME:-"${_REMOTE_USER:-"dev"}"}"
 HOME_DIR=$( getent passwd "$USERNAME" | cut -d: -f6 )
 
 echo "Downloading Dotfiles..."
-cd $HOME_DIR
-git clone https://github.com/OliRafa/dotfiles.git 
-cd dotfiles
+sudo -u $USERNAME bash <<EOF
+    cd $HOME_DIR
+    git clone https://github.com/OliRafa/dotfiles.git
+    cd dotfiles
 
-# By using the combo `--adopt` with `git reset --hard` we can replace files that
-# are already in the filesystem with files from the repository.
-stow . --adopt
-git reset --hard
+    # By using the combo `--adopt` with `git reset --hard` we can replace files that
+    # are already in the filesystem with files from the repository.
+    stow . --adopt
+    git reset --hard
+EOF
 
 echo "Setting up LazyVim and Mason..."
 nvim --headless "+Lazy! sync" +qa
