6ff67d6d27a803d45c2917920f4c1cf7a776551e
Rafael Augusto de Oliveira <olirafa@proton.me>
2025-12-04 23:17:38 +0000

    fix(neovim-setup): run stow as non-root user

--- src/neovim-setup/install.sh
+++ src/neovim-setup/install.sh
@@ -14,7 +14,7 @@ cd dotfiles
 
 # By using the combo `--adopt` with `git reset --hard` we can replace files that
 # are already in the filesystem with files from the repository.
-stow . --adopt
+runuser -l $USERNAME -- stow . --adopt
 git reset --hard
 
 echo "Setting up LazyVim and Mason..."
