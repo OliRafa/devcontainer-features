46e6a03e1b715175635f3ae6b956c58004d33108
Rafael Augusto de Oliveira <olirafa@proton.me>
2025-12-05 18:38:19 +0000

    feat(zsh): add transient prompt

--- src/zsh-setup/install.sh
+++ src/zsh-setup/install.sh
@@ -8,6 +8,7 @@ USERNAME="${USERNAME:-"${_REMOTE_USER:-"dev"}"}"
 echo "Installing zsh-autosuggestions..."
 sudo -u $USERNAME bash <<EOF
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone --depth=1 https://codeberg.org/olets/zsh-transient-prompt.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/zsh-transient-prompt
 EOF
 
 echo "Finished setting up Zsh!"
