60011ff31b8cfa9772fd7b83d9c59f131e6d4a3e
Rafael Augusto de Oliveira <olirafa@proton.me>
2026-05-07 14:09:51 +0100

    refactor(nvim): reuse dotfiles feature

--- src/neovim-setup/install.sh
+++ src/neovim-setup/install.sh
@@ -5,19 +5,9 @@ set -e
 echo "Starting to setup Neovim..."
 
 USERNAME="${USERNAME:-"${_REMOTE_USER:-"dev"}"}"
-HOME_DIR=$( getent passwd "$USERNAME" | cut -d: -f6 )
 
 echo "Downloading Dotfiles..."
 sudo -u $USERNAME bash <<EOF
-    cd $HOME_DIR
-    git clone https://github.com/OliRafa/dotfiles.git
-    cd dotfiles
-
-    # By using the combo `--adopt` with `git reset --hard` we can replace files that
-    # are already in the filesystem with files from the repository.
-    stow . --adopt
-    git reset --hard
-
     echo "Setting up LazyVim and Mason..."
     nvim --headless "+Lazy! sync" +qa
 EOF
